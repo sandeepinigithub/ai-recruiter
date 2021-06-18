501aa6c955fb9262323ec3269af89bc30f5bb30a
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-06-19 02:04:55 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email : string;
+  d:any;
+
+  constructor(private loginService: LoginService , private route :Router) { }
+
+  ngOnInit(): void {
+  }
+  login() {
+    this.loginService.loginCredential({"email" : this.email}).subscribe(data=>{
+      // console.log(data);
+      this.d =data ;
+      if(this.d.message == "Wrong email or password"){
+        alert(this.d.message);
+      }
+      else{
+        if(this.d.data.id != null){
+          // console.log("not null");
+          this.route.navigate(['home']);                  
+        }
+      }
+      
+      
+    })
+    
+  }
+
+}
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  loginCredential(loginData:any){
+    return this.http.post('https://ai-recruiter.herokuapp.com/api/company/login',loginData);
+  }
+}
